6d93d83a8a4932b175710e7fab07632a229f213a
MortezaOM <mortezamohammady56@gmail.com>
2023-06-13 23:30:11 +0430

    do all at once

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
-import 'package:flutter/material.dart';
 import 'package:clima/screens/loading_screen.dart';
+import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
